ec495a9e694d2b814446e89210e4da5fae96689d
Sanjib234 <sanjib234@gmail.com>
2017-08-09 22:00:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+   res.send("Article one requested and will be served here") 
+});
+
+app.get('/article-Two',function(req,res){
+   res.send("Article two requested and will be served here") 
+});
+
+app.get('/article-Three',function(req,res){
+   res.send("Article theree requested and will be served here") 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
