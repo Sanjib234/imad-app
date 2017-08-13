001c98bdf4b5fac05de14a0d8e02df95e2ee114d
Sanjib234 <sanjib234@gmail.com>
2017-08-13 20:10:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,42 +22,7 @@ var articleOne = {
     
 };
 
-function createTemplate(data){
-    var title = data.title;
-    var heading = data.heading;
-    var date = data.date;
-    var content = data.content;
-    
-    var htmlTemplate=`
-    <html>
-        <head>
-            <title>
-                ${title}
-            </title>
-            <meta name ="veiwport" content = "width=device-width, initial-scale=1" />
-             <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-            <div class="container">
-            
-                <div>
-                    <o herf="/">Home</o>
-                </div>
-                    <hr/>
-                       <h3>
-                        ${heading}
-                    </h3>
-                <div>
-                     ${date}
-                </div>
-                <div>
-                     ${content}
-                </div>
-            </div>
-        
-    </html>
-    `;
-    return htmlTemplate;
-};
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
