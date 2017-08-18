1c002127db46f9183e2f63df78b3bd586b087397
Sanjib234 <sanjib234@gmail.com>
2017-08-18 21:11:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName',function(req,res){
     //artcileName=article-one
     //articles[articleName] = {}content object for article one
-    var articleName = req.params.articleName
+    var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
